1abda95154420629e6cab5dc190d21e237acf110
Diego Collares <diego.collares@gmail.com>
2024-10-19 10:16:56 -0300

    sidebar continue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 
 import Header from './components/header'
-import { Home } from './features/home'
 import { Post } from './features/Post'
 
 import './styles/global.css'
+
+import styles from './App.module.css' 
+import { Sidebar } from './components/Sidebar'
 
 
 export function App() {
@@ -11,6 +13,12 @@ export function App() {
   return (
     <>
         <Header />
+        <div className={styles.wrapper}>
+         <Sidebar />
+         <main>
+            <Post title="Apenas um título" description="apenas uma descrição de texto etc" />
+         </main>
+        </div>
     </>
   )
 }
