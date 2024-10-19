b217b36e938c314a61a5c3834172e62a1745e994
Diego Collares <diego.collares@gmail.com>
2024-10-19 09:21:54 -0300

    styled header and add logo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 import styles from './Header.module.css'
 
+import ignitelogo from '../../assets/logo.svg'
+
 export default function Header() {
   return (
-    <header className={styles.header}>Header</header>
+    <header className={styles.header}>
+      <img src={ignitelogo} alt="Ignite Logo" />
+    </header>
   )
 }
